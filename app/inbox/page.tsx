cc8335d86337d20044e7474373f8168c5bdd869d
SuperExe404 <diegomon0440@gmail.com>
2025-02-23 00:54:09 -0600

    Inbox page

---
+++ app/inbox/page.tsx
@@ -0,0 +1,15 @@
+import Conversation from "../components/inbox/Conversation";
+
+const InboxPage = () => {
+    return (
+        <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
+                <h1 className="text-2xl my-6">Inbox</h1>
+
+                <Conversation />
+                <Conversation />
+                <Conversation />
+        </main>
+    )
+}
+
+export default InboxPage;
