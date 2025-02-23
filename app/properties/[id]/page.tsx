f8e2ce81bd57c21581b1c28cc0e20c00855af0f5
SuperExe404 <diegomon0440@gmail.com>
2025-02-22 21:00:47 -0600

    Detail page for properties

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
+
+const PropertyDetailPage = () => {
+    return(
+        <main className="max-w-[1500px] mx-auto px-6 pb-6">
+            <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+                <Image
+                fill
+                src='/beach_1.jpg'
+                className="objects-cover w-full h-full" 
+                alt="each House"
+                />
+            </div>
+
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+                <div className="py-66 pr-6 col-span-3">
+                    <h1 className="mb-4 text-4xl">Property name</h1>
+
+                    <span className="mb-6 block text-lg text-gray-600">
+                        4 guests - 2 bedrooms - 1 bathroom
+                    </span>
+
+                    <hr />
+
+                    <div className="oy-6 flex items-center space-x-4">
+                        <Image
+                            src="/profile_pic_1.jpg"
+                            width={50}
+                            height={50}
+                            className="rounded-full"
+                            alt="The user name"
+                        />
+
+                        <p><strong>Diego Montes</strong> is your host</p>
+                    </div>
+
+                    <hr />
+                    <p className="mt-6 text-lg">
+                        asdfghjklqwertyuiopzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm
+                    </p>
+                </div>
+
+                <ReservationSidebar />
+            </div>
+        </main>
+    )
+}
+
+export default PropertyDetailPage;
