dcdb8b44dc120f573ec40c0f770e4a16aff36d81
SuperExe404 <diegomon0440@gmail.com>
2025-03-17 21:01:00 -0600

    Implement authentication sgunup

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -1,29 +1,73 @@
 'use client'
 
 import Modal from "./Modal";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import useSignupModal from "../hooks/useSignupModal";
 import CustomButton from "../forms/CustomButton";
+import apiService from "@/app/services/apiServices";
+import { handleLogin } from "@/app/lib/actions";
+ 
 
 const SignupModal = () => {
     const signupModal = useSignupModal()
+    const router = useRouter();
+     const [email, setEmail] = useState('');
+     const [password1, setPassword1] = useState('');
+     const [password2, setPassword2] = useState('');
+     const [errors, setErrors] = useState<string[]>([]);
+     
+ 
+     const submitSignup = async () => {
+         const formData = {
+             name: 'Santiago',
+             email: email,
+             password1: password1,
+             password2: password2
+         }
+ 
+         const response = await apiService.post('/api/auth/register/', JSON.stringify(formData));
+ 
+         if (response.access) {
+             handleLogin(response.user.pk, response.access, response.refresh);
+ 
+             SignupModal.close();
+ 
+             router.push('/')
+         } else {
+             const tmpErrors: string[] = Object.values(response).map((error: any)=>{
+                 return error;
+             })
+ 
+             setErrors(tmpErrors);
+         }
+     }
     const content = (
         <>
-            <form className="space-y-4">
-                <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+            <form 
+                 action={submitSignup}
+                 className="space-y-4">
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                <input placeholder="Your Password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                <input placeholder="Pepeat your  Password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+                 <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    The error mesage
-                </div>
+                 { errors.map((error, index) =>{
+                     return(
+                         <div 
+                             key={`error_${index}`}
+                             className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+ 
+                         >
+                             { error}         
+                         </div>
+                     )
+                 })}
 
                 <CustomButton
                     label="Submit"
-                    onClick={() => console.log('test')}
-                />
+                    onClick={submitSignup}/>
             </form>
         </>
     )
--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,54 +1,41 @@
 'use client';
-
-import { useEffect, useState } from "react";
-import PropertylistItem from "./PropertyListItem";
-import apiService from "@/app/services/apiServices";
-
-export type PropertyType = {
-    id: string;
-    title: string;
-    image_url: string;
-    price_per_night:number;
-}
-
-const Propertylist = () => {
-    const [properties, setProperties] = useState<PropertyType[]>([]);
-
-    const getProperties = async () => {
-        const url = 'http://localhost:8000/api/properties/properties/';
-
-        await fetch(url, {
-            method: 'GET',
-        })
-        .then(response => response.json())
-        .then((json) => {
-            console.log('json', json)
-
-            setProperties(json.data)
-        })
-        .catch((error) => {
-            console.log('error', error)
-        })
-    };
-
-    useEffect(() => {
-        apiService.get('hello')
-
-        getProperties();
-    }, []);
-
-    return (
-        <>
-            {properties.map((property) => {
-                return(
-                    <PropertylistItem 
-                        key={property.id}
-                        property={property}
-                    />
-                )
-            })}
-        </>
-    )
-}
-
-export default Propertylist;
+ 
+ import apiService from '@/app/services/apiServices';
+ import { useEffect, useState } from 'react';
+ 
+ import PRopertyListItem from "./PropertyListItem";
+ 
+ export type PropertyType = {
+     id: string;
+     title: string;
+     image_url: string;
+     price_per_night: number;
+ }
+ 
+ const PropertyList = ()=>{
+     const[properties,setProperties]=useState<PropertyType[]>([]);
+ 
+     const getProperties = async () =>{
+         const tmpProperties = await apiService.get('/api/properties/')
+ 
+         console.log(tmpProperties);  // Verifica la respuesta
+         setProperties(tmpProperties.data || []); 
+         setProperties(tmpProperties.data);
+     };
+     useEffect(()=>{
+         getProperties();
+     },[]);
+ 
+     return(
+         <>
+             {properties.map((property)=>{
+                 return(
+                     <PRopertyListItem
+                         key={property.id}
+                         property={property}
+                     />
+                 )
+             })}
+         </>
+     )
+ }
